121a47e232c0f4c2871fc57702e321e8d194448a
Filipe Gabriel Fagundes <filipe.g.fagundes@gmail.com>
2015-09-01 12:22:08 -0300

    Alterado para criar a pasta de downlads caso não exista

--- downloader.php
+++ downloader.php
@@ -21,6 +21,9 @@ $conn_id = ftp_connect($ftp_server);
 // login with username and password
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
+// Checks if the download folder exists, if not, creates it
+if ( !is_writable($destiny_folder) ) mkdir($destiny_folder);
+
 // try to download $server_file and save to $local_file
 if (ftp_get($conn_id, $local_file, $server_file, FTP_BINARY)) {
     fwrite($log, $date."Arquivo $local_file salvo com sucesso".PHP_EOL);
--- exec.bat
+++ exec.bat
@@ -1,2 +1,2 @@
-c:\xampp\php\php -f C:\Users\Filipe\Documents\projetos\php-bots\test.php
+c:\xampp\php\php -f C:\Users\Filipe\Documents\projetos\php-bots\downloader.php
 timeout 60
